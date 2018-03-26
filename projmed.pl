7eff60f12dff646b85d24ec8211bf7f8ebec520b
Jodowy <jodowy@gmail.com>
2018-03-26 09:54:45 +0200

    Rodzaje choroby dzialaja

--- projmed.pl
+++ projmed.pl
@@ -35,7 +35,6 @@ start:-
     abolish(known/3),
     dynamic(known/3),
     retractall(known/3),
-    type,
     diagnose, nl, nl,
     write('Chcesz sprobowac ponownie ? (t/n)'), nl,
     read(Resp),\+ Resp=t, nl,
@@ -44,10 +43,7 @@ start:-
 
 notice:-
     write('Czesc jak masz na imie?'), nl,
-    read_line_to_codes(user_input,Codes1),
-    capitalize(Codes1,Codes2)
-    ->
-    atom_codes(Imie,Codes2),
+    read(Imie),
     write(Imie).
 
 symptom(P, Val):- nl, ask('Czy odczuwasz symptom taki jak: ',Val).
@@ -56,23 +52,16 @@ ask(Pytanie, Val):- known(Pytanie, Val, true),!.
 ask(Pytanie, Val):- known(Pytanie, Val, false),!, fail.
 
 ask(Pytanie, Val):-
-    write(Pytanie),write(Val) , write('? (t/n)'), nl,
+    write(Pytanie), write(Val), write('? (t/n)'), nl,
     read(Ans), !,
     ((Ans=t, assert(known(Pytanie, Val, true)));
     (assert(known(Pytanie, Val, false)),fail)).
 
-type:- rodzaj(Rodzaj).
+diagnose:-
+nl, hipoteza(symptom,Disease),
+!, nl,
+write('Moze to byc '),
+write(Disease).
 
 diagnose:-
-    nl, hipoteza(symptom,Disease),
-    !, nl,
-    write('Moze to byc '),
-    write(Disease).
-
-diagnose:-
-    nl, write('Niestety nie wiem co to za choroba').
-
-capitalize([],[]).
-capitalize([H1|T], [H2|T]):-
-    code_type(H2, to_upper(H1)).
-
+nl, write('Niestety nie wiem co to za choroba').
